d77f49f485b88b639154f8f6315ff145be520bd0
Aaron Garrett <aaron@readyfundgo.com>
2021-09-27 09:33:15 -0400

    Update test.php

--- public/api/test.php
+++ public/api/test.php
@@ -1,1 +1,54 @@
 <?php
+
+$num = 2;
+$foo = $num ." be";
+$bar = "or not " .$num. " be";
+
+echo $foo . ' ' . $bar . "\n";
+
+echo $num * $num * $num;
+
+$arr = [
+    "first" => "Tom",
+    "second" => "Bipin",
+    "best" => "DS"
+];
+
+$arr2 = [1,1,2,3,5,8];
+
+if (true) {
+    echo "\nTRUE\n";
+}
+
+while (true) {
+    //This way it doesn't actually do anything
+    break;
+}
+
+#This is also a comment
+/* This is a
+multi-line
+comment */
+
+// echo "<ul>";
+// foreach($arr as $key=>$val) {
+//     echo "<li>".$key ." is ".$val."</li>";
+// }
+// echo "</ul>";
+
+function printAndEncode( $val ) {
+
+    echo json_encode (
+        $val, 
+        JSON_PRETTY_PRINT| /* This Bar is the Binary "OR" */ JSON_THROW_ON_ERROR
+    );
+}
+
+//====
+//Naming Conventions
+
+//JS & PHP : camelCase -- You should really try to stick with this with everything
+
+//PascalCase -- Used for classes most typically
+//snake_case
+//kebab-case
