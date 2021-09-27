08be6807e9b08153ce2610acb4ea0cea55082cd7
Aaron Garrett <aaron@readyfundgo.com>
2021-09-27 08:58:11 -0400

    k

--- public/js/index.js
+++ public/js/index.js
@@ -32,24 +32,26 @@ const Offer = {
     },
 
     methods: {
-
+        fetchUserData(){
+            fetch('https://randomuser.me/api')
+        
+            .then( response => response.json())
+    
+            .then((json) => {
+                console.log(json);
+                this.result = json.results[0];
+            }
+            )
+    
+            .catch( (error) => {
+    
+                    console.error(error);
+            })
+        }
     },
 
     created() {
-        fetch('https://randomuser.me/api')
-        
-        .then( response => response.json())
-
-        .then((json) => {
-            console.log(json);
-            this.result = json.results[0];
-        }
-        )
-
-        .catch( (error) => {
-
-                console.error(error);
-        })
+        this.fetchUserData();
     }
 }
 
